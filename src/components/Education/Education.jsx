e27bec481cf6aed70f75f4b03b6457a0ebeff5d0
AlekseyVY <alekseyvy@gmail.com>
2021-09-10 16:58:35 +0300

    feat education section revision draft

--- src/components/Education/Education.jsx
+++ src/components/Education/Education.jsx
@@ -1,10 +1,52 @@
-import {AnimationWrapper, Container} from "./education.styles";
+import {AnimationWrapper, CodeWrapper, Container} from "./education.styles";
 import LottiePlayer from "../lottiePlayer/LottiePlayer";
-
+import Highlight from 'react-highlight';
 
 const Education = () => {
+
   return(
     <Container id="educationID">
+      <CodeWrapper>
+        <Highlight className='javascript'>
+          {"class Education {\n" +
+          "  constructor() {\n" +
+          "    this.name = \"Aleksey Vasiliev\";\n" +
+          "    this.russian = 'Native';\n" +
+          "    this.english = \"C2\";\n" +
+          "  }\n" +
+          "\n" +
+          "  bachelor = () => {\n" +
+          "    const name = \"Bachelor of Science: Computer Science\";\n" +
+          "    const university = \"University of People\";\n" +
+          "    const timeLine = \"2018-08 - current.\";\n" +
+          "  }\n" +
+          "\n" +
+          "  Certificate_1 = () => {\n" +
+          "    const name = \"Full Stack open\";\n" +
+          "    const issuer = \"University of Helsinki\";\n" +
+          "    const timeLine = \"10/2019\";\n" +
+          "  }\n" +
+          "\n" +
+          "  Certificate_2 = () => {\n" +
+          "    const name = \"JavaScript Algorithms and Data Structures\";\n" +
+          "    const issuer = \"FreeCodeCamp\";\n" +
+          "    const timeLine = \"02/2020\";\n" +
+          "  }\n" +
+          "\n" +
+          "  Certificate_3 = () => {\n" +
+          "    const name = \"CS50\";\n" +
+          "    const issuer = \"Harvard University\";\n" +
+          "    const timeLine = \"11/2020\";\n" +
+          "  }\n" +
+          "\n" +
+          "  Certificate_4 = () => {\n" +
+          "    const name = \"EF SET\";\n" +
+          "    const issuer = \"efset.org\";\n" +
+          "    const timeLine = \"12/2020 \";\n" +
+          "  }\n" +
+          "}"}
+        </Highlight>
+      </CodeWrapper>
       <AnimationWrapper>
         <LottiePlayer data={'./lotties/education.json'} name={'education'}/>
       </AnimationWrapper>
--- src/components/Education/education.styles.js
+++ src/components/Education/education.styles.js
@@ -9,15 +9,27 @@ export const Container = styled.section`
   min-height: 100vh;
   display: flex;
   align-items: center;
-  justify-content: center;
+  justify-content: flex-start;
 `;
 
 export const AnimationWrapper = styled.div`
-  max-width: 900px;
+  max-width: 750px;
   width: 100%;
   position: absolute;
-  right: -10rem;
   bottom: 0;
+  right: 0;
   pointer-events: none;
   z-index: -2;
+`;
+
+export const CodeWrapper = styled.aside`
+  width: 700px;
+  font-size: 1.9rem;
+  margin: 6rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  backdrop-filter: blur(10px) saturate(180%);
+  -webkit-backdrop-filter: blur(10px) saturate(180%);
+  background-color: rgba(255, 255, 255, 0.55);
 `;
